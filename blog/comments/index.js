30fc6debfc561c958c88e77e31f119407843808a
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-08-23 14:51:13 +0200

    wip2 mini-microservice app

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,16 +1,16 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const {randomBytes} = require("crypto")
+const cors = require("cors")
 
 
 const app = express()
 app.use(bodyParser.json())
-
+app.use(cors())
 const commentsByPostId = {}
 
-app.get("/posts/:id/comments", (res, req) => {
-    res.send(commentsByPostId[req.params.id] || [])
-    
+app.get("/posts/:id/comments", (req, res) => {
+    res.send(commentsByPostId[req.params.id || []])
 }) 
 
 app.post("/posts/:id/comments", (req, res) => {
